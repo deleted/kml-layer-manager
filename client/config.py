584e5d1df724598393b361cced628fcb648e1e96
Ted Scharff <e.b.scharff@nasa.gov>
2010-12-11 15:57:31 -0800

    Explain config.py

--- client/config.py
+++ client/config.py
@@ -1,3 +1,10 @@
+"""
+config.py sets up connection options and is loaded
+by layers.py when the server connection is established.
+
+It's best to put your actual connection options in local_config.py, which is
+excluded from version control.  Variable defined there will overwrite the defaults here.
+"""
 cms_connection_options = (
     'somehost',     #host
     'someuser',     #username
